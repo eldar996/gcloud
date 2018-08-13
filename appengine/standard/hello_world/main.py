ff6d2b940b7d70ed74225987b2aa82a0044a21dc
Eldar Takyrbashev <ETakyrbashev@meas-inc.com>
2018-08-13 15:43:20 -0400

    Initial

--- appengine/standard/hello_world/main.py
+++ appengine/standard/hello_world/main.py
@@ -14,13 +14,28 @@
 
 import webapp2
 
+form="""
+<form method="post" action="/testform">
+    <input name="q">
+    <input type="submit">
+</form>
+"""
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        #self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(form)
+
+class TestHandler(webapp2.RequestHandler):
+    def post(self):
+        # q=self.request.get("q")
+        # self.response.out.write(q)
+
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.out.write(self.request)
+
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', MainPage),('/testform', TestHandler)
 ], debug=True)
